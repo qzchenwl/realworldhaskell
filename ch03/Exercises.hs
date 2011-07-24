9ad094adbd78d7b4bfb0fb61575be9567ca56b49
Wenlong <qzchenwl@gmail.com>
2011-07-24 10:48:37 +0800

    GrahamScan finished

--- ch03/Exercises.hs
+++ ch03/Exercises.hs
@@ -16,10 +16,17 @@ sortByLength' :: [[a]] -> [[a]]
 sortByLength' []    = []
 sortByLength' (p : xs) = (sortByLength' lesser) ++ [p] ++ (sortByLength' greater)
     where
-        lesser   = filter (lt p) xs
-        greater  = filter (ge p) xs
-        lt xs ys = length ys < length xs
-        ge xs ys = length ys >= length xs
+        lesser   = filter (gt p) xs
+        greater  = filter (le p) xs
+        gt xs ys = length xs >  length ys
+        le xs ys = length xs <= length ys
+
+sortBy' :: (a -> a -> Ordering) -> [a] -> [a]
+sortBy' cmp []       = []
+sortBy' cmp (p : xs) = (sortBy' cmp lesser) ++ [p] ++ (sortBy' cmp greater)
+    where
+        lesser  = filter (\x -> cmp x p == LT) xs
+        greater = filter (\x -> (cmp x p == GT) || cmp x p == EQ) xs
 
 intersperse' :: a -> [[a]] -> [a]
 intersperse' s (x:xs)
@@ -38,7 +45,7 @@ height' Empty        = 0
 data Direction' = Left'
                 | Right'
                 | Straight'
-                deriving (Show)
+                deriving (Eq, Show)
 
 turn' (ax, ay) (bx, by) (cx, cy)
         | crossProduct > 0  = Left'
@@ -51,3 +58,21 @@ turn' (ax, ay) (bx, by) (cx, cy)
 
 turnList' (a:b:c:xs) = (turn' a b c) : turnList' (b:c:xs)
 turnList' _          = []
+
+
+-- sort points first by y, if equals sort by x
+compareP' (x1, y1) (x2, y2) = if not (y1 == y2) then (compare y1 y2) else (compare x1 x2)
+
+-- cosine of angle line (p1, p2) make with x-axis
+cos' (x1, y1) (x2, y2) = (x2 - x1) / (sqrt ((x2 - x1)^2 + (y2 - y1)^2))
+
+compareAngle (x1, y1) (x2, y2) (x3, y3) = compare (cos' (x1, y1) (x3, y3)) (cos' (x1, y1) (x2, y2))
+
+grahamScan list = scan [] sortedList ++ [head sortedList]
+    where
+        sortedList = (head list) : (sortBy' (compareAngle lowestP) (tail list))
+        lowestP    = head (sortBy' compareP' list)
+        scan preResult (p1:p2:p3:leftResult)
+            | turn' p1 p2 p3 /= Right' = scan (p1:preResult) (p2:p3:leftResult)
+            | otherwise                = scan (tail preResult) ((head preResult):p2:p3:leftResult)
+        scan preResult ps              = []
---
+++ ch03/GrahamScan.hs
@@ -0,0 +1,60 @@
+-- file: ch03/GrahamScan.hs
+
+import Data.List
+
+data Point = Point Double Double
+             deriving (Eq, Show)
+
+data Direction = LeftTurn
+               | RightTurn
+               | Straight
+                 deriving (Eq, Show)
+
+cross :: Point -> Point -> Point -> Double
+cross (Point x1 y1) (Point x2 y2) (Point x3 y3) = (x2-x1)*(y3-y1) - (x3-x1)*(y2-y1)
+
+dist :: Point -> Point -> Double
+dist (Point x1 y1) (Point x2 y2) = sqrt((x1-x2)^2+(y1-y2)^2)
+
+turn :: Point -> Point -> Point -> Direction
+turn pvt a b
+    | crossProduct >  0 = LeftTurn
+    | crossProduct <  0 = RightTurn
+    | crossProduct == 0 = Straight
+    where
+        crossProduct = cross pvt a b
+
+compareByY :: Point -> Point -> Ordering
+compareByY (Point x1 y1) (Point x2 y2)
+    | y1 /= y2  = compare y1 y2
+    | otherwise = compare x1 y1
+
+compareByAngle :: Point -> Point -> Point -> Ordering
+compareByAngle pvt a b
+    | angle /= EQ = angle
+    | otherwise   = distance
+    where
+        angle = compare 0 (cross pvt a b)
+        distance = compare (dist pvt a) (dist pvt b)
+
+lowestY :: [Point] -> Point
+lowestY = minimumBy compareByY
+
+grahamScan :: [Point] -> [Point]
+grahamScan ps = scan [pvt] sortedList
+    where
+        sortedList = (sortBy (compareByAngle pvt) ((delete pvt ps))) ++ [pvt]
+        pvt        = lowestY ps
+        scan preResult (a:b:left)
+            | direction /= RightTurn = scan (a:preResult) (b:left)
+            | otherwise          = scan (tail preResult) (prePoint:b:left)
+            where direction = turn prePoint a b
+                  prePoint  = head preResult
+        scan preResult _    = preResult
+
+tupleListToPoints :: [(Double,Double)] -> [Point]
+tupleListToPoints xs = map (\(x, y) -> (Point x y)) xs
+
+pointsToTupleList :: [Point] -> [(Double, Double)]
+pointsToTupleList xs = map (\(Point x y) -> (x, y)) xs
+
