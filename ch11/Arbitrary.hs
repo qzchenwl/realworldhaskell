7037666dbe3bd373a1697310a0aeb0c321a4d34b
Wenlong <qzchenwl@gmail.com>
2011-08-04 21:55:31 +0800

    ch11: from official site

--- ch11/Arbitrary.hs
+++ ch11/Arbitrary.hs
@@ -1,35 +1,48 @@
--- file: ch11/Arbitrary.hs
-
 module Arbitrary where
 
-import Test.QuickCheck
-
-{-
+{-- snippet Class --}
 class Arbitrary a where
-    arbitrary   :: Gen a
-    elements    :: [a] -> Gen a
-    choose      :: Random a => (a, a) -> Gen a
-    oneof       :: [Gen a] -> Gen a
--}
+  arbitrary   :: Gen a
+{-- /snippet Class --}
 
+--  coarbitrary :: a -> Gen b -> Gen b
+
+{-- snippet IntroductionForms --}
+  elements :: [a] -> Gen a
+  choose   :: Random a => (a, a) -> Gen a
+  oneof    :: [Gen a] -> Gen a
+{-- /snippet IntroductionForms --}
+
+{-- snippet ternary --}
 data Ternary
     = Yes
     | No
     | Unknown
-    deriving (Eq, Show)
+    deriving (Eq,Show)
+{-- /snippet ternary --}
 
-{-
+{-- snippet Instance --}
 instance Arbitrary Ternary where
-    arbitrary   = elements [Yes, No, Unknown]
--}
+  arbitrary     = elements [Yes, No, Unknown]
+{-- /snippet Instance --}
 
-instance Arbitrary Ternary where
-    arbitrary   = do
-        n <- choose (0, 2) :: Gen Int
-        return $ case n of
-                      0 -> Yes
-                      1 -> No
-                      2 -> Unknown
+{-- snippet InstanceProduct --}
+instance (Arbitrary a, Arbitrary b) => Arbitrary (a, b) where
+  arbitrary = do
+      x <- arbitrary
+      y <- arbitrary
+      return (x, y)
+{-- /snippet InstanceProduct --}
 
-instance Arbitrary Char where
-    arbitrary = elements (['A'..'Z'] ++ ['a'..'z'] ++ " ~!@#$%^&*()")
+{-- snippet InstanceRec --}
+instance Arbitrary a => Arbitrary [a] where
+  arbitrary =
+      n <- choose (0, 1) :: Gen Int
+      if n == 0
+         then []
+         else do x  <- arbitrary :: a
+                 xs <- arbitrary :: Gen [a]
+                 return (x : xs)
+{-- /snippet InstanceRec --}
+
+--  coarbitrary b = if b then variant 0 else variant 1
--- ch11/QC-basics.hs
+++ ch11/QC-basics.hs
@@ -1,35 +1,51 @@
--- file: ch11/QC-basics.hs
-
+{-- snippet module --}
 import Test.QuickCheck
 import Data.List
+{-- /snippet module --}
 
+-- Simple model testing
+
+{-- snippet mysort --}
 qsort :: Ord a => [a] -> [a]
-qsort []        = []
-qsort (x:xs)    = qsort lhs ++ [x] ++ qsort rhs
-    where lhs = filter (< x) xs
+qsort []     = []
+qsort (x:xs) = qsort lhs ++ [x] ++ qsort rhs
+    where lhs = filter  (< x) xs
           rhs = filter (>= x) xs
+{-- /snippet mysort --}
 
+{-- snippet idempotent --}
 prop_idempotent xs = qsort (qsort xs) == qsort xs
+{-- /snippet idempotent --}
 
-prop_minimum xs = head (qsort xs) == minimum xs
+{-- snippet relatives_wrong --}
+prop_minimum xs         = head (qsort xs) == minimum xs
+{-- /snippet relatives_wrong --}
 
-prop_minimum' xs = not (null xs) ==> head (qsort xs) == minimum xs
+{-- snippet relatives_right --}
+prop_minimum' xs         = not (null xs) ==> head (qsort xs) == minimum xs
+{-- /snippet relatives_right --}
 
+{-- snippet relatives --}
 prop_ordered xs = ordered (qsort xs)
-    where ordered []        = True
-          ordered [x]       = True
-          ordered (x:y:xs)  = x <= y && ordered (y:xs)
+    where ordered []       = True
+          ordered [x]      = True
+          ordered (x:y:xs) = x <= y && ordered (y:xs)
 
 prop_permutation xs = permutation xs (qsort xs)
     where permutation xs ys = null (xs \\ ys) && null (ys \\ xs)
 
-prop_maximum xs     =
+prop_maximum xs         =
     not (null xs) ==>
         last (qsort xs) == maximum xs
 
-prop_append xs ys   =
+prop_append xs ys       =
     not (null xs) ==>
     not (null ys) ==>
         head (qsort (xs ++ ys)) == min (minimum xs) (minimum ys)
+{-- /snippet relatives --}
 
-prop_sort_model xs  = sort xs == qsort xs
+{-- snippet model --}
+prop_sort_model xs      = sort xs == qsort xs
+{-- /snippet model --}
+
+-- Generating random data
